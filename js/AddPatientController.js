e994fe8681d4e5969fc292d1b362d5a85eb42f90
Rohit Nigam <rohit.nigam@cerner.com>
2015-10-11 00:49:18 +0530

    adding disease section

--- js/AddPatientController.js
+++ js/AddPatientController.js
@@ -1,17 +1,18 @@
 angular
 .module('ngJohnny')
 .controller('AddPatientController', function($scope, $firebaseArray){
-
+$scope.addDetails = true;
 $scope.newPatient = {};
 var ref = new Firebase("https://flickering-torch-1357.firebaseio.com/patients");
 $scope.patients=$firebaseArray(ref);
+$scope.newPatient.date = new Date().toLocaleString();
 
 $scope.addPatient = function(newPatient){
 
 $scope.patients.$add(newPatient).then(function(){
   console.log('successfully written');
 });
-
+$scope.newPatient = {};
 }
 
 
