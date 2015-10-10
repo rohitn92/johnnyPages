154cfc2728799e9b35f897951b99cc95db0b9296
Rohit Nigam <rohit.nigam@cerner.com>
2015-10-10 19:40:11 +0530

    firebase works

--- js/SearchController.js
+++ js/SearchController.js
@@ -1,14 +1,23 @@
 angular
 .module('ngJohnny')
-.controller('SearchController', function($scope, PatientFactory){
+.controller('SearchController', function($scope, $firebaseArray){
 
-$scope.patients;
+//$scope.patients;
 
-PatientFactory.getPatients().success(function(data){
-  $scope.patients = data;
-}).error(function(error){
-  console.log(error);
-});
+ var ref = new Firebase("https://flickering-torch-1357.firebaseio.com/patients");
+// var syncObject = $firebaseObject(ref);
+
+// syncObject.$bindTo($scope, "patients");
+
+$scope.patients=$firebaseArray(ref);
+
+// PatientFactory.getPatients().success(function(data){
+//   $scope.patients = data;
+// }).error(function(error){
+//   console.log(error);
+// });
+
+
 
 
 });
