562d949f72be725ec3b16cdf25ee8179cdc96e1a
Rohit Nigam <rohit.nigam@cerner.com>
2015-10-11 01:51:20 +0530

    add details almost done

--- js/AddPatientController.js
+++ js/AddPatientController.js
@@ -2,10 +2,23 @@ angular
 .module('ngJohnny')
 .controller('AddPatientController', function($scope, $firebaseArray){
 $scope.addDetails = true;
+$scope.addDiseases = false;
 $scope.newPatient = {};
 var ref = new Firebase("https://flickering-torch-1357.firebaseio.com/patients");
 $scope.patients=$firebaseArray(ref);
 $scope.newPatient.date = new Date().toLocaleString();
+
+
+  $scope.newPatient.diseases = [{type:'', comments: ''}];
+
+  $scope.removeFrom = function(array, index) {
+    array.splice(index, 1);
+  };
+
+  $scope.addTo = function(array, template) {
+    array.push(template);
+  };
+
 
 $scope.addPatient = function(newPatient){
 
@@ -13,6 +26,10 @@ $scope.patients.$add(newPatient).then(function(){
   console.log('successfully written');
 });
 $scope.newPatient = {};
+$scope.newPatient.date = new Date().toLocaleString();
+$scope.newPatient.diseases = [{type:'', comments: ''}];
+$scope.addDetails = true;
+$scope.addDiseases = false;
 }
 
 
